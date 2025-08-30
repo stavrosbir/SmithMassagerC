b06effb252070adf3443c4d84f5114d4c577866d
Ziwen Wang <ziwenwang@Ziwens-MacBook-Air.local>
2025-08-29 20:54:33 -0400

    [FLINT] Support flint v3.3.*

--- src/core/specialIntCert.c
+++ src/core/specialIntCert.c
@@ -12,15 +12,15 @@ int specialIntCert(fmpz_mat_t ret, fmpz_t s, fmpz_mat_t A, fmpz_mat_t B, int n, 
   // AA[1..n, 1..n] = A[1..n, 1..n]
   // AA[1..n, -r..-1] := A[1..n, -r..-1]
   for (int i = 0; i < n; ++i) {
-    _fmpz_vec_set(BB->rows[i], B->rows[i], B->c);
-    _fmpz_vec_set(AA->rows[i], A->rows[i], n);
-    _fmpz_vec_set(AA->rows[i] + AA->c - r, A->rows[i] + A->c - r, r);
+    _fmpz_vec_set(fmpz_mat_entry(BB, i, 0), fmpz_mat_entry(B, i, 0), B->c);
+    _fmpz_vec_set(fmpz_mat_entry(AA, i, 0), fmpz_mat_entry(A, i, 0), n);
+    _fmpz_vec_set(fmpz_mat_entry(AA, i, 0) + AA->c - r, fmpz_mat_entry(A, i, 0) + A->c - r, r);
   }
   // AA[-r..-1, 1..n] := A[-r..-1, 1..n]
   // AA[-r..-1, -r..-1] := A[-r..-1, -r..-1]
   for (int i = 1; i <= r; ++i) {
-    _fmpz_vec_set(AA->rows[n+r-i], A->rows[2*n-i], n);
-    _fmpz_vec_set(AA->rows[n+r-i] + AA->c - r, A->rows[2*n-i] + A->c - r, r);
+    _fmpz_vec_set(fmpz_mat_entry(AA, n+r-i, 0), fmpz_mat_entry(A, 2*n-i, 0), n);
+    _fmpz_vec_set(fmpz_mat_entry(AA, n+r-i, 0) + AA->c - r, fmpz_mat_entry(A, 2*n-i, 0) + A->c - r, r);
   }
 
   int success = fmpzFastIntCert(s, res, AA, BB, n+r, m);
@@ -31,11 +31,11 @@ int specialIntCert(fmpz_mat_t ret, fmpz_t s, fmpz_mat_t A, fmpz_mat_t B, int n, 
 
   // ret[1..n, 1..m] := res[1..n, 1..m]
   for (int i = 0; i < n; ++i) {
-    _fmpz_vec_set(ret->rows[i], res->rows[i], m);
+    _fmpz_vec_set(fmpz_mat_entry(ret, i, 0), fmpz_mat_entry(res, i, 0), m);
   }
   // ret[-r..-1, 1..m] := res[-r..-1, 1..m]
   for (int i = 1; i <= r; ++i) {
-    _fmpz_vec_set(ret->rows[2*n-i], res->rows[n+r-i], m);
+    _fmpz_vec_set(fmpz_mat_entry(ret, 2*n-i, 0), fmpz_mat_entry(res, n+r-i, 0), m);
   }
 
 clear:
